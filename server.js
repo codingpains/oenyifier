9bb3c2c5be97872a1e4c5ee2cac2a7b270372c80
Gus Ortiz <guspidey@gmail.com>
2015-09-07 06:34:16 -0500

    add cors support

--- server.js
+++ server.js
@@ -32,9 +32,19 @@ var multipart = function(req, done) {
   req.pipe(busboy);
 };
 
+var cors = function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, CONNECT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Max-Age', '86400');
+}
+
 var requestHandler = function(req, res) {
   var path = url.parse(req.url).pathname;
   console.log('Request to ' + req.method + ' ' + path);
+
+  cors(req, res);
 
   if (req.method === 'POST' && req.headers['content-type']) {
     multipart(req, function() {
